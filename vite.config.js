be67d28005b597e1a0890caf4cd85392c543cefd
anthonymateo <anthonymateodelosreyes.18@gmail.com>
2025-06-28 11:53:21 -0600

    arreglos

--- vite.config.js
+++ vite.config.js
@@ -13,26 +13,15 @@ export default defineConfig({
       },
       shared: ['react', 'react-dom'],
     }),
-    {
-      name: 'vite-plugin-notify-host-on-rebuild',
-      apply(config, { command }) {
-        return Boolean(command === 'build' && config.build?.watch);
-      },
-      async buildEnd(error) {
-        if (!error) {
-          try {
-            await fetch('http://localhost:5000/__fullReload');
-          } catch (e) {
-            console.log(e);
-          }
-        }
-      },
-    },
   ],
   build: {
     modulePreload: false,
     target: 'esnext',
     minify: false,
     cssCodeSplit: false,
+  },
+  preview: {
+    port: 5001,
+    strictPort: true,
   },
 });
